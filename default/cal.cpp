8fd1160e48c6622bbfa4ade1d8383fe9e93e98f6
jinhong <jh0703.kim@gmail.com>
2024-07-02 16:35:04 +0900

    update getSumSum

--- default/cal.cpp
+++ default/cal.cpp
@@ -1,4 +1,8 @@
 class Cal
 {
-
+public:
+	int getSumSum(int a, int b, int c) 
+	{
+		return (a + b + c);
+	}
 };
--- default_test/test.cpp
+++ default_test/test.cpp
@@ -5,3 +5,9 @@ TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
+
+TEST(TestCaseName, getSumSum) {
+	Cal c;
+	int retult = c.getSumSum(1, 2, 3);
+	EXPECT_EQ(retult, 6);
+}
