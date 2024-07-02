a7566d6070b1ffb75315c3af16cbbf090c31e67b
Heeseung Yoo <heeseung.yoo@samsung.com>
2024-07-02 16:30:07 +0900

    implement getGop()

--- default_test/test.cpp
+++ default_test/test.cpp
@@ -5,3 +5,14 @@ TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
+
+TEST(TestCaseName, getGop) {
+	auto cal = Cal{};
+
+	EXPECT_EQ(1, cal.getGop(1, 1));
+	EXPECT_EQ(2, cal.getGop(1, 2));
+	EXPECT_EQ(4, cal.getGop(2, 2));
+	EXPECT_EQ(10000, cal.getGop(100, 100));
+	EXPECT_EQ(7006652, cal.getGop(1234, 5678));
+	EXPECT_NE(97408265472, cal.getGop(123456, 789012));
+}
